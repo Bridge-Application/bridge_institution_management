ab92f53c0168b9f70cabdd73f5c98bd134ce6173
Isaac Tong <isaact943@outlook.com>
2020-09-05 20:34:55 -0500

    Created add user page, creating success page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 
 app.get('/add/user', (req, res) => {
-    res.render('test');
+    res.render('user_form');
+})
+
+app.post('/add/user', (req, res) => {
+  console.log(req.body);
+  res.render('created_user');
 })
 
 app.listen(port, () => {
